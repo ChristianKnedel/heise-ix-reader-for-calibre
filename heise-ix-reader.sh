d87fbc87cfd9ed1668d5551ab8cf5368d7a8b7fb
Herr Knedel <himself@christian-knedel.de>
2020-04-18 12:44:57 +0200

    Update heise-ix-reader.sh

--- heise-ix-reader.sh
+++ heise-ix-reader.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
+#apt-get update && apt-get install -y xpdf calibre
+export LANG=C.UTF-8
 mkdir /tmp/worker1/
 
-find /books/ -type f -iname '*.pdf'  -print0 | 
+find /books/ -type f -iname 'IX*.pdf'  -print0 | 
 while IFS= read -r -d '' line; do 
         calibreID=$(echo  "$line" | sed -r 's/.*\(([0-9]+)\).*/\1/g')
         
@@ -14,13 +16,11 @@ while IFS= read -r -d '' line; do
         pdftotext -f 0 -l 10 /tmp/worker1/test.pdf /tmp/worker1/tmp.txt
 
         echo "text aufbereitung"
-        cat /tmp/worker1/tmp.txt | sed ':begin;N;s/ /\n/;tbegin' | grep -iE '[A-Za-z]{2,212}' | grep  -i -F -w -v -f  blacklist.txt |  sed -r s/[^a-zA-Z0-9]+/-/g | sed ':begin;$!N;s/\n/,/;tbegin' > /tmp/worker1/final.txt
+        cat /tmp/worker1/tmp.txt  | grep  -i -F -w -v -f  /books/blacklist.txt | sed -r s/[^a-zA-ZäöüÄÖÜ]+//g | grep -iE '[A-Za-z]{2,212}' |  sed ':begin;$!N;s/\n/,/;tbegin' > /tmp/worker1/final.txt
 
         echo "aktualisier db"
-        calibredb set_metadata --with-library=/books/ --field tags:"$(cat /tmp/worker1/final.txt) " $calibreID
-        rm /tmp/worker1/*
+        calibredb set_metadata --with-library=/books/ --field tags:"$(cat /tmp/worker1/final.txt) " --field series:"Heise IX" --field languages:"Deutsch" --field authors:"Hansa Verlag" $calibreID
+        
+        #rm /tmp/worker1/*
    
 done
-
-
-
